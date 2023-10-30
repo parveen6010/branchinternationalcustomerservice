00a5d1c277819f36e57df8c092a27042635eb097
parveen prajapati <77872164+parveen6010@users.noreply.github.com>
2023-10-30 14:52:03 +0530

    int

--- backend/server.js
+++ backend/server.js
@@ -37,11 +37,11 @@ app.use("/api/agent", agentRoutes);
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frntend/build")));
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frntend", "build", "index.html"))
-  );
+  app.get("*", (req, res) =>{
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  });
 } else {
   app.get("/", (req, res) => {
     res.send("API is running..");
